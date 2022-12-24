98ef090356527702f81137d9c3df9b83e516a161
LAPTOP-Q59EOFNB\Ganesh Tangudu <ganeshtangudu123@gmail.com>
2022-12-24 12:22:22 +0530

    core java programs

---
+++ practiecing_programs_core java/Scale.java
@@ -0,0 +1,13 @@
+class Scale 
+{
+	public static boolean result=true;
+	public static boolean status=false;
+	public static void main(String[] args) 
+	{
+		System.out.println(result);
+		System.out.println(status);
+	}
+}
+			
+			
+			
